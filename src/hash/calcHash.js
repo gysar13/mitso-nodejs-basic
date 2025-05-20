17fcad69638b915f5f27bbcb822324ab07dfcae5
gysar13 <gysar1133@gmail.com>
2025-05-21 01:42:48 +0300

    hash ready

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => hash.update(chunk));
+    stream.on('end', () => {
+      console.log(hash.digest('hex'));
+      resolve();
+    });
+    stream.on('error', reject);
+  }); 
 };
 
 await calculateHash();
