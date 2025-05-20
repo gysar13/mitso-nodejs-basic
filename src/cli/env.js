efb666779747e61fde60045214daab0286c6a03f
gysar13 <gysar1133@gmail.com>
2025-05-21 01:40:10 +0300

    cli ready

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envs = Object.entries(process.env)
+      .filter(([key]) => key.startsWith('MITSO_'))
+      .map(([key, value]) => `${key}=${value}`);
+  
+    console.log(envs.join('; ')); 
 };
 
 parseEnv();
