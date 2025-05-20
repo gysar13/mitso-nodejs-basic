0477b65d6a37c54b9e41f64e606c4aae63ece037
gysar13 <gysar1133@gmail.com>
2025-05-21 01:41:18 +0300

    cp ready

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,26 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    if (!Array.isArray(args)) {
+        throw new TypeError('args should be an array');
+    }
+    
+    const child = spawn('node', ['files/script.js', ...args], {
+        stdio: [process.stdin, process.stdout, process.stderr, 'ipc']
+    });
+
+    child.on('message', (message) => {
+        console.log(`Message from child: ${message}`);
+    });
+
+    process.on('message', (message) => {
+        const result = performCalculation(message);
+        child.send(result);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const performCalculation = (data) => {
+    return `Calculated result for ${data}`;
+};
+
+spawnChildProcess(['arg1', 'arg2']);
